968c50fc948655f84b5e78cef9ea2b754b610238
Mihaela CARVAL <balan.myhaela@yahoo.com>
2024-11-26 21:05:56 +0100

    feature: node, express and MongoDB Atlas database configuration

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json()); // lire le body des rêquetes en json
+
+mongoose.connect('mongodb+srv://Mia:Mongodb18.@cluster0.uzelg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+module.exports = app;
